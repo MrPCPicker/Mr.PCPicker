d14e2419c8bbe6e12eb39224229dad0cfdaa03d9
JoosunLee <judycanada11@gmail.com>
2025-12-16 19:14:44 +0900

    gms연동

--- pickerapp/views.py
+++ pickerapp/views.py
@@ -6,6 +6,21 @@ from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework import status
 
+
+#gms 연결
+from .services.gms_client import call_gms_openai
+
+
+@api_view(["GET"])
+def gms_test(request):
+    text = call_gms_openai(
+        "나에게 자존감을 올려줄 말을 해줘"
+    )
+    return Response({
+        "result": text
+    })
+
+
 # Create your views here.
 # @api_view(['GET'])
 # def post_list(request):
