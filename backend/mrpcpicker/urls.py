028fc1da32705ee22f2bff8250e3254a678f0b82
JoosunLee <judycanada11@gmail.com>
2025-12-21 17:57:49 +0900

    251221프로필페이지

--- backend/mrpcpicker/urls.py
+++ backend/mrpcpicker/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('gms/', include('gms.urls')),
 ]
+
+if settings.DEBUG:
+    # 개발 환경에서 /media/ 요청을 MEDIA_ROOT(업로드 폴더)에서 찾아서 응답
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
